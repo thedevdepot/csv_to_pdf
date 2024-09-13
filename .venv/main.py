45b49d83d2d70d7e8a8fb7dedeeabadd78f1512c
Python Mega Course <tonyringertx@gmail.com>
2024-09-12 22:06:46 -0500

    Add more pages for each topic.

--- .venv/main.py
+++ .venv/main.py
@@ -10,6 +10,8 @@ for index, row in df.iterrows():
     pdf.set_text_color(10, 10, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 22, 200, 22)
-
+    
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
 
 pdf.output("output.pdf")
